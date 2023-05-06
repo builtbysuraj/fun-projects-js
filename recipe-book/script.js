cc0c3c3277ee263d67472ce45ec297246604304a
builtbysuraj <surajgupta3553@gmail.com>
2023-05-07 01:45:07 +0530

    Finished recipe-book

---
+++ recipe-book/script.js
@@ -0,0 +1,51 @@
+const API_KEY = "01d2aa8b7a324d67ae66dbeddf7290db";
+const recipeListEl = document.getElementById("recipe-list");
+
+function displayRecipes(recipes) {
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-item");
+    recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe image";
+
+    recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerText = recipe.title;
+
+    recipeIngredientsEl = document.createElement("p");
+    recipeIngredientsEl.innerHTML = `
+        <strong>Ingredients:</strong> ${recipe.extendedIngredients
+        .map((ingredient) => ingredient.original)
+        .join(", ")}
+    `;
+
+    recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.innerText = "View Recipe";
+
+    recipeItemEl.appendChild(recipeImageEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientsEl);
+    recipeItemEl.appendChild(recipeLinkEl);
+    recipeListEl.appendChild(recipeItemEl);
+  });
+}
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.recipes;
+}
+
+async function init() {
+  const recipes = await getRecipes();
+  console.log(recipes)
+  displayRecipes(recipes);
+}
+
+init();
